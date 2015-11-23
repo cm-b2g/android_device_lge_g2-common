cm-b2g/android_device_lge_g3-common

add some local prefs for Gecko

prefs/tiles.js
@@ -1,0 +1,2 @@
+pref("layers.tile-width", 320);
+pref("layers.tile-height", 360);

cm-b2g/android_device_lge_g2-common

prefs/tiles.js
@@ -1,0 +1,2 @@
+pref("layers.tile-width", 360);
+pref("layers.tile-height", 320);